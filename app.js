d55c5b801f0f49a7b14a60853fbf33cd1ab90518
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-02-28 22:36:57 +0400

    Baseline design for form pages complete
    This design will be used for all form pages. The only thing that will change is text/title and "form-group".

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// Start questionnaire by hiding the welcome page.
 function startQuest() {
     let mainSection = document.getElementById('main-section');
     mainSection.style.display = 'none';
+}
+
+
+var activePage = 1;
+
+
+if(activePage = 1) {
+    document.getElementById('elipse1').style.opacity = 1;
 }
